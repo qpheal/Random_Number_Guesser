5b8b67b6af608edf69af66943188c792cff38eec
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-20 08:31:34 +0000

    Update

--- GuessNumber.js
+++ GuessNumber.js
@@ -4,16 +4,25 @@ const prompt = require("prompt-sync")()
 // Generate a random number
 // To create a random no from 1 to 100
 const target = Math.round(Math.random() * 100)   // Math.random generates a random number between 0 to 1
-console.log(target)
+//console.log(target)
 // We add to the guesses anytime user makes a guess
 let guesses = 0
 
-const guess = Number(prompt("Guess the number (0-100:"))
+
+// To repeat till the users guesses the right answer use the while loop
+while(true) { // The true makes it infinite so you got to create a condition to get it false
+//Keeping track of guesses
+    guesses++
+    const guess = Number(prompt("Guess the number (0-100:"))
 // NOte the value return to guess is always a string 
 if (guess > target){
     console.log("Your guesss is high")
-} else if(guess===target){
-    console.log("You got it!!!")
+} else if(guess < target){
+    console.log("Your guess is low")
 } else{
-    console.log("You guess is too low")
+    console.log("You guessed it!")
+    break// This exits the  closest internal loop
 }
+}
+
+console.log("You guessed it in ",guesses,"tries")
